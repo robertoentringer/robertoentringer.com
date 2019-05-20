e79e5aa78e762a837e966c65e8341b2d6654bb0f
Roberto Entringer <r.entringer@gmail.com>
2019-05-20 17:07:54 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["prettier", "html"],
+  rules: {
+    "prettier/prettier": ["error", { semi: false, singleQuote: false }],
+    "no-console": "off",
+  },
+}
