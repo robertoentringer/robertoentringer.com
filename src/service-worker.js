1e9cfcc526691f9890fe9e2814f95fabf1dfdfae
Roberto Entringer <r.entringer@gmail.com>
2019-06-19 03:30:57 +0200

    remove console.log

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,9 +5,6 @@ self.addEventListener("install", e =>
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(urlsToCache)))
 )
 
-self.addEventListener("fetch", e => {
-  console.log(e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  )
-})
+self.addEventListener("fetch", e =>
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)))
+)
