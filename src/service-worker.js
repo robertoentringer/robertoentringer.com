b8e11dd02833998178dc585b396e1bdb5d8f4162
Roberto Entringer <r.entringer@gmail.com>
2019-12-02 21:55:53 +0100

    implements service worker clear  old cache version

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,10 +1,22 @@
-const cacheName = "29-nov-2019-v3"
+const cacheVersion = "2-december-2019"
 const urlsToCache = ["/", "/manifest.json"]
 
 self.addEventListener("install", e =>
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(urlsToCache)))
+  e.waitUntil(caches.open(cacheVersion).then(cache => cache.addAll(urlsToCache)))
 )
 
 self.addEventListener("fetch", e =>
   e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)))
 )
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== cacheVersion) return caches.delete(cacheName)
+        })
+      )
+    )
+  )
+})
