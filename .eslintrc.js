baba8d3d41746f2fc038fd132ae3ddb842168990
Roberto Enringer <r.entringer@gmail.com>
2020-07-12 22:44:13 +0200

    fix conflict between eslint prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es6: true },
+  env: { browser: true, es6: true, commonjs: true },
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: { ecmaVersion: 2018 },
   plugins: ["prettier", "html"],
