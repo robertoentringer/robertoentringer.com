1479e902e9d9072c1f8002bf9c3f16ceec534190
Roberto Enringer <r.entringer@gmail.com>
2020-07-12 22:58:54 +0200

    fix and run lint script

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,19 @@
 const cacheVersion = "2-december-2019"
 const urlsToCache = ["/", "/manifest.json"]
 
-self.addEventListener("install", e =>
-  e.waitUntil(caches.open(cacheVersion).then(cache => cache.addAll(urlsToCache)))
+self.addEventListener("install", (e) =>
+  e.waitUntil(caches.open(cacheVersion).then((cache) => cache.addAll(urlsToCache)))
 )
 
-self.addEventListener("fetch", e =>
-  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)))
+self.addEventListener("fetch", (e) =>
+  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)))
 )
 
-self.addEventListener("activate", function(event) {
+self.addEventListener("activate", function (event) {
   event.waitUntil(
-    caches.keys().then(cacheNames =>
+    caches.keys().then((cacheNames) =>
       Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheName !== cacheVersion) return caches.delete(cacheName)
         })
       )
